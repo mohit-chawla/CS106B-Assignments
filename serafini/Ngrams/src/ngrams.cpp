d4975655cd8f278fb1911a355343b1088ab57867
Mohit Chawla <mohitchawla@cse.ism.ac.in>
2017-05-30 20:33:07 +0530

    serafini added

---
+++ serafini/Ngrams/src/ngrams.cpp
@@ -0,0 +1,124 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "console.h"
+#include "filelib.h"
+#include "simpio.h"
+#include "hashmap.h"
+#include "Vector.h"
+#include "random.h"
+
+
+using namespace std;
+
+static string INPUT_FILE_NAME_PROMPT =  "Input file name? ";
+static string BAD_FILE_REPROMPT = "Unable to open that file.  Try again.";
+static string N_INPUT_PROMPT = "Value of N? ";
+static string N_INPUT_ERR_MSG = "N must be 2 or greater.";
+static string NUM_WORDS_PROMPT = "# of random words to generate (0 to quit)? ";
+static string EXIT_MSG = "Exiting.";
+
+Vector<string> getWindow(Vector<string> allWords, int start, int size){
+    Vector<string> window;
+    int end = allWords.size();
+    for(int i=0;i<size;i++){
+        if(start+i>=end)
+            start = -i;
+        window.add(allWords.get(start+i));
+    }
+    cout<<"window:"<<window.toString()<<endl;
+    return window;
+}
+string nextWord(Vector<string> allWords, int start){
+    return allWords.get(start%allWords.size());
+}
+void printIntro(){
+    cout<<"Welcome to CS 106B Random Writer ('N-Grams')."<<endl;
+    cout<<"This program makes random text based on a document."<<endl;
+    cout<<"Give me an input file and an 'N' value for groups"<<endl;
+    cout<<"of words, and I'll create random text for you."<<endl;
+    cout<<endl;
+}
+
+int main() {
+    printIntro();
+    string ipFileName;
+    int N; //N-grams
+    int numWords = -1;
+    ipFileName = getLine(INPUT_FILE_NAME_PROMPT);
+    while (!fileExists(ipFileName)) {
+      // case when user-specified file DNE
+      cout << BAD_FILE_REPROMPT << endl;
+      ipFileName = getLine(INPUT_FILE_NAME_PROMPT);
+    }
+    HashMap<Vector<string>,Vector<string>> hmap;
+    Vector<string> allWords;
+    string word;
+    ifstream fin(ipFileName);
+    while(fin>>word){
+        allWords.add(word);
+    }
+
+    N = getInteger(N_INPUT_PROMPT);
+    while(N<2){
+        cout<<N_INPUT_ERR_MSG<<endl;
+        N = getInteger(N_INPUT_PROMPT);
+    }
+    N=4;
+    for(int i=0;i<allWords.size();i++){
+        Vector<string> window = getWindow(allWords,i,N-1);
+        string nextword = nextWord(allWords,i+N-1);
+        Vector<string> temp;
+        if(hmap.containsKey(window)){
+            temp = hmap.get(window);
+        }
+        temp.add(nextword);
+        hmap.put(window,temp);
+    }
+    while(numWords!=0){
+        cout<<endl;
+        numWords = getInteger(NUM_WORDS_PROMPT);
+        while(numWords<N && numWords!=0){
+            cout<<"Must be at least "<<N<<" words."<<endl;
+            numWords = getInteger(NUM_WORDS_PROMPT);
+            if(numWords ==0){
+                break;
+            }
+        }
+        if(numWords!=0){
+            cout<<"... ";
+            int startKey = randomInteger(0,hmap.size()-1);
+            Vector<Vector<string>> allKeys  = hmap.keys();
+            int wordsLeft = numWords;
+            Vector<string> initialRandString = allKeys.get(startKey);
+            for(int i=0;i<initialRandString.size();i++){
+                if(i==initialRandString.size()-1)
+                    cout<<initialRandString.get(i);
+                else
+                    cout<<initialRandString.get(i)<<" ";
+            }
+            wordsLeft -= N-1;
+            Vector<string> thisWindow = allKeys.get(startKey);
+            while(wordsLeft>0){
+                Vector<string> nextWordVector = hmap.get(thisWindow);
+                //cout<<nextWordVector.toString()<<endl;
+                int rand = randomInteger(0,nextWordVector.size()-1);
+                string newWord = nextWordVector.get(rand);
+                cout<<" "<<newWord;
+                //slide the window
+                thisWindow.remove(0);
+                thisWindow.add(newWord);
+                wordsLeft--;
+            }
+            cout<<" ..."<<endl;
+        }
+    }
+    cout << "Exiting." << endl;
+    return 0;
+}
---
+++ serafini/WordLadder/src/wordladder.cpp
@@ -0,0 +1,216 @@
+/*
+ * author: Mohit Chawla | mc2683@cornell.edu | Cornell Tech Connective Media
+ * Program
+ * decription:CS106b-assignment-2-serafini-wordladder-http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1164//assn/serafini.html
+ * date: 29 May 2017
+ */
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "console.h"
+#include "simpio.h"
+#include "stack.h"
+#include "queue.h"
+#include "filelib.h"
+#include "hashmap.h"
+
+using namespace std;
+
+//STATIC-CONSTANTS
+static string DICTIONARY_FILE_NAME_PROMPT = "Dictionary file name? ";
+static string BAD_FILE_REPROMPT = "Unable to open that file.  Try again.";
+static string WORD_1_PROMPT = "Word #1 (or Enter to quit):";
+static string WORD_2_PROMPT = "Word #2 (or Enter to quit):";
+static string INVALID_INPUT_MSG = "Invalid input.";
+static string TERMINATION_MSG = "Have a nice day.";
+static string SAME_LEN_ERR_MSG = "The two words must be the same length.";
+static string DICT_DNE_ERR_MSG = "The two words must be found in the dictionary.";
+static string DUPLICATE_WRODS_ERR_MSG = "The two words must be different.";
+
+/*
+* Test if a word exists in the dictionary
+* @param
+*   word:string
+*   dictMap:HashMap<string,bool>&
+* @return
+*   bool
+*/
+bool existsInDictionary(string word,HashMap<string,bool> &dictMap){
+   return dictMap.containsKey(word);
+}
+
+/*
+* Loads the dictionary into the HashMap
+* @param
+*   ipFileName:string
+*   in:ifstream
+*   dictMap:HashMap<string,bool>&
+* @return
+*
+*/
+void loadDictionary(string ipFileName,std::ifstream &in, HashMap<string,bool> &dictMap){
+    string thisWord;
+    openFile(in, ipFileName.c_str());
+    while(!in.eof()){
+        getLine(in, thisWord);
+        dictMap.add(thisWord,false);
+    }
+}
+
+/*
+* Used for input validation
+* @param
+*   word1:string
+*   word2:string
+*   dictMap:HashMap<string,bool>
+* @return
+*   bool
+*/
+bool validInput(string word1, string word2,HashMap<string,bool> &dictMap){
+    if(equalsIgnoreCase(word1,word2)){
+        cout<<DUPLICATE_WRODS_ERR_MSG<<endl;
+        return false;
+    }
+    if(word1.length()!=word2.length()){
+        cout<<SAME_LEN_ERR_MSG<<endl;
+        return false;
+    }
+    if(!existsInDictionary(word1,dictMap) || !existsInDictionary(word2,dictMap)){
+        cout<<DICT_DNE_ERR_MSG<<endl;
+        return false;
+    }
+       return true;
+}
+
+/*
+* Copies the stack
+* @param
+*   stack:HashMap<string,bool>&
+* @return
+*   HashMap<string,bool>
+*/
+Stack<string> stackCopy(Stack<string> stack){
+    return stack;
+}
+
+/*
+* Prints the ladder using the stack
+* @param
+*   stack:HashMap<string,bool>
+* @return
+*/
+void printAnswer(Stack<string> stack){
+    //cout<<"Size of path: "<<stack.size()<<endl;
+    while(!stack.isEmpty()){
+        cout<<stack.pop()<<" ";
+    }
+    cout<<endl;
+}
+
+/*
+* Encapsulates reset operations to be done after each Tc
+* @param
+*   usedKeys:Vector<string>&
+*   dictMap:HashMap<string,bool>&
+* @return
+*/
+void testCaseReset(Vector<string>& usedKeys, HashMap<string,bool>&dictMap){
+    int size = usedKeys.size();
+    for(int i=0;i<size;i++){
+        dictMap.put(usedKeys.get(i),false);
+    }
+    usedKeys.clear();
+}
+
+/*
+* Prints program introduction
+* @param
+* @return
+*/
+void printIntro(){
+    cout<<"Welcome to CS 106B Word Ladder."<<endl;
+    cout<<"Please give me two English words, and I will change the"<<endl;
+    cout<<"first into the second by changing one letter at a time."<<endl;
+    cout<<endl;
+}
+
+int main() {
+
+    string ipFileName;
+    string word1;
+    string word2;
+    HashMap<string,bool> dictMap ;
+    Vector<string> usedKeys;
+    ifstream in;
+
+    printIntro();
+    ipFileName = getLine(DICTIONARY_FILE_NAME_PROMPT);
+    while (!fileExists(ipFileName)) {
+      // case when user-specified file DNE
+      cout << BAD_FILE_REPROMPT << endl;
+      ipFileName = getLine(DICTIONARY_FILE_NAME_PROMPT);
+    }
+    //load the dictionary from file into HashMap
+    loadDictionary(ipFileName,in,dictMap);
+
+    while(1){
+        word1 = getLine(WORD_1_PROMPT);
+        if(word1.length()==0)
+            break;
+        word1 = toLowerCase(word1);
+        word2 = getLine(WORD_2_PROMPT);
+        if(word2.length()==0)
+            break;
+        word2 = toLowerCase(word2);
+        if(validInput(word1,word2,dictMap)){
+            int length = (int)word1.length();
+            Queue<Stack<string>> queue;
+            Stack<string> s;
+            s.add(word1);
+            queue.add(s);
+            usedKeys.add(word1);
+            bool solved = false;
+            while(!queue.isEmpty() && !solved){
+               Stack<string> partialLadder = queue.dequeue();
+               string topWord = partialLadder.top();
+               for(int i=0;i<length;i++){
+                   for(int j=97;j<123;j++){
+                       if(topWord[i]!=(char)j){
+                           string temp = topWord;
+                           temp[i] = (char)j;
+                           if(existsInDictionary(temp,dictMap) && !dictMap.get(temp)){
+                               //cout<<"topWord,temp: "<<topWord<<","<<temp<<". ";
+                                //word exists in dict and unused;
+                                dictMap.put(temp,true); //use this word
+                                usedKeys.add(temp);
+                                if(equalsIgnoreCase(temp,word2)){
+                                    //cout<<"hurray soln is found"<<endl;
+                                    cout<<"A ladder from "<<word2<<" back to "<<word1<<":"<<endl;
+                                    cout<<word2<<" ";
+                                    printAnswer(partialLadder);
+                                    solved =true;
+                                    break;
+                                }else{
+                                    Stack<string> partialLadderCopy = stackCopy(partialLadder);
+                                    //cout<<"copiedStack is"<<partialLadderCopy.toString()<<endl;
+                                    partialLadderCopy.push(temp);
+                                    queue.enqueue(partialLadderCopy);
+                                }
+                           }
+                       }
+                   }
+               }
+            }
+            if(!solved){
+                cout<<"No word ladder found from "<<word2<<" back to "<<word1<<"."<<endl;
+            }
+        }
+        //reset operations after each Tc
+        testCaseReset(usedKeys,dictMap);
+    }
+    cout << TERMINATION_MSG << endl;
+    return 0;
+}
