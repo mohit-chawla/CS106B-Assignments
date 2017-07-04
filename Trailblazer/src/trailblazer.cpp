c7e72a7ab3ac37b64bcb3dbbf004858875b4bfa2
Mohit Chawla <mohitchawla@cse.ism.ac.in>
2017-07-05 00:01:16 +0530

    DFS done

--- Trailblazer/src/trailblazer.cpp
+++ Trailblazer/src/trailblazer.cpp
@@ -5,7 +5,8 @@
  */
 #include "trailblazer.h"
 #include "queue.h"
-
+#include "hashmap.h"
+#include "stack.h"
 using namespace std;
 
 //Function to get index of an element in a vector
@@ -21,76 +22,109 @@ int getElementIndex(Vertex* elem, Vector<Vertex*> vec){
     return -1;
 }
 
-void dfs(BasicGraph& graph, Vertex* start, Vertex* end, Vector<Vertex*> &path){
+bool dfs(BasicGraph& graph, Vertex* start, Vertex* end, Vector<Vertex*> &path, HashMap<Vertex*,bool> visited){
    //add v1 to path and mark as visited -> GREEN
-   path.add(start);
    start->setColor(GREEN);
-//   start->visited = true;
-   graph.getVertex(start->name)->visited = true;
    if(start == end){
-       cout<<"YAY";
-       return;
+       return true;
    }else{
         Set<Vertex*> neighbours = graph.getNeighbors(start);
         Set<Vertex*>::iterator n;
         for(n=neighbours.begin();n!=neighbours.end();++n){
             Vertex* thisNeighbour = *n;
             cout<<thisNeighbour->name<<endl;
-            if(!thisNeighbour->visited){
-                dfs(graph,thisNeighbour,end,path);
+            if(!visited.containsKey(thisNeighbour)){
+                visited.add(thisNeighbour,true);
+                if(dfs(graph,thisNeighbour,end,path,visited)){
+                    path.add(thisNeighbour);
+                    return true;
+                }
+                //remove element from path -> GREY
+                thisNeighbour->setColor(GRAY);
+                visited.remove(thisNeighbour);
             }
-            //remove element from path -> GREY
-            start->setColor(GRAY);
-//            start->visited = false;
-//            path.remove(getElementIndex(start,path));
+
         }
    }
-   return;
+   return false;
 }
 
 Vector<Vertex*> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     //reset graph before starting
     graph.resetData();
     Vector<Vertex*> path;
-    dfs(graph,start,end,path);
-    return path;
+    HashMap<Vertex*,bool> visited;
+    visited.add(start,true);
+    dfs(graph,start,end,path,visited);
+    //Path contains reverse elements
+    Vector<Vertex*> pathToReturn;
+    pathToReturn.add(start);
+    for(int i=path.size()-1;i>=0;i--)
+        pathToReturn.add(path[i]);
+    return pathToReturn;
 }
 
-void bfs(BasicGraph& graph, Vertex* start, Vertex* end){
+//ITERATIVE
+//Vector<Vertex*> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
+//    //reset graph before starting
+//    graph.resetData();
+//    Vector<Vertex*> path;
+//    Stack<Vertex *> stack;
+//    stack.push(start);
+//    while(!stack.isEmpty()){
+//        Vertex* v = stack.pop();
+
+//        if(!v->visited){
+//            v->setColor(GREEN);
+//            v->visited = true;
+//            path.add(v);
+//        }
+//        if(v==end){
+//            return path;
+//        }
+//        Set<Vertex*> neighbours = graph.getNeighbors(v);
+//        Set<Vertex*>::iterator n;
+//        for(n=neighbours.begin();n!=neighbours.end();++n){
+//            Vertex* thisNeighbour = *n;
+//            if(!thisNeighbour->visited){
+//                stack.push(thisNeighbour);
+//            }
+//       }
+
+//    }
+
+//    return path;
+//}
+Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
+    //reset graph before starting
+    graph.resetData();
+
+    Set<Vertex*> vertices = graph.getVertexSet();
+    Set<Vertex*>::iterator n;
+    Vector<Vertex*> path;
     Queue<Vertex *> queue;
     queue.add(start);
     start->visited = true;
     while(!queue.isEmpty()){
         Vertex *v = queue.dequeue(); //->GREEN
         v->setColor(GREEN);
+        path.add(v);
+        cout<<"Dequeued"<<v->name<<endl;
         if(v == end){
-            return;
+            return path;
         }
-        Set<Vertex*> neighbours = graph.getNeighbors(start);
+        Set<Vertex*> neighbours = graph.getNeighbors(v);
         Set<Vertex*>::iterator n;
         for(n=neighbours.begin();n!=neighbours.end();++n){
             //for each neighbour of this vertex
             Vertex* thisNeighbour = *n;
-            thisNeighbour->visited = true; //mark as visited
-            queue.add(thisNeighbour); //add to queue -->YELLOW
-            thisNeighbour->setColor(YELLOW);
+            if(!thisNeighbour->visited){
+                thisNeighbour->visited = true; //mark as visited
+                queue.add(thisNeighbour); //add to queue -->YELLOW
+                cout<<"Queued:"<<thisNeighbour->name<<endl;
+                thisNeighbour->setColor(YELLOW);
+            }
         }
-    }
-}
-
-Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    //reset graph before starting
-    graph.resetData();
-    bfs(graph,start,end);
-    Set<Vertex*> vertices = graph.getVertexSet();
-    Set<Vertex*>::iterator n;
-    Vector<Vertex*> path;
-
-    //construct path
-    for(n=vertices.begin();n!=vertices.end();++n){
-        Vertex* v = *n;
-        if(v->getColor() == GREEN)
-            path.add(v);
     }
     return path;
 }
